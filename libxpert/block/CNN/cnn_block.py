7afee2f5f25a6286bf0e3676cf53da72cd66578f
rachmanz <abdurrahman.121450128@student.itera.ac.id>
2025-05-03 12:57:38 +0700

    CBAM Module

---
+++ libxpert/block/CNN/cnn_block.py
@@ -0,0 +1,18 @@
+##########################
+#   CNN Block Extension  #
+#       (Modular)        #
+##########################
+
+# === CBAM Block Process ===
+class CBAM(nn.Module):
+    def __init__(self, gate_channels, reduction_ratio=16, pool_types=['avg', 'max'], no_spatial=False):
+        super(CBAM, self).__init__()
+        self.ChannelGate = ChannelGate(gate_channels, reduction_ratio, pool_types)
+        self.no_spatial = no_spatial
+        if not no_spatial:
+            self.SpatialGate = SpatialGate()
+    def forward(self, x):
+        x_out = self.ChannelGate(x)
+        if not self.no_spatial:
+            x_out = self.SpatialGate(x_out)
+        return x_out
